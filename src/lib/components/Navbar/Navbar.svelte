af4e9a571d3416a7317da8e24fbdd5eaea2c0262
ajl0023 <austinlee199723@yahoo.com>
2022-05-06 19:30:31 -0700

    updated nav links

--- src/lib/components/Navbar/Navbar.svelte
+++ src/lib/components/Navbar/Navbar.svelte
@@ -1,24 +1,56 @@
 <script context="module">
 	import Github from '$lib/svgs/navIcons/github.svelte';
 	import Gmail from '$lib/svgs/navIcons/gmail.svelte';
-	import Linkedin from '$lib/svgs/navIcons/LinkedIn.svelte';
-
+	import LinkedIn from '$lib/svgs/navIcons/LinkedIn.svelte';
 	import Resume from '$lib/svgs/navIcons/resume.svelte';
-	const icons = [Github, Gmail, Linkedin, Resume];
+	import { onMount } from 'svelte';
+
+	const icons = [
+		{
+			link: 'https://github.com/ajl0023',
+			component: Github,
+			download: null
+		},
+		{
+			link: 'mailto:austinjlee.0@gmail.com',
+			component: Gmail,
+			download: null
+		},
+
+		{
+			link: 'https://www.linkedin.com/in/austin-lee-91a341215/',
+			component: LinkedIn,
+			download: null
+		},
+
+		{
+			link: './AustinLeeResume.pdf',
+			component: Resume,
+			download: 'AustinLeeResume.pdf'
+		}
+	];
 </script>
 
 <script>
-	import { onMount, tick } from 'svelte';
-
 	onMount(async () => {});
 </script>
 
 <div class="wrapper">
 	<div class="icons-container">
-		{#each icons as Icon}
-			<div class="icon-container">
-				<Icon />
-			</div>
+		{#each icons as item}
+			<a
+				on:click="{() => {
+					if (!item.download) {
+						window.open(item.link, '_blank');
+					}
+				}}"
+				download="{item.download}"
+				rel="external"
+				href="{item.download ? item.link : null}"
+				class="icon-container"
+			>
+				<svelte:component this="{item.component}" />
+			</a>
 		{/each}
 	</div>
 </div>
@@ -27,7 +59,7 @@
 	.wrapper {
 		position: fixed;
 		top: 0;
-
+		right: 0;
 		margin: auto;
 		z-index: 20;
 		display: flex;
@@ -39,11 +71,13 @@
 		}
 		.icons-container {
 			display: flex;
+
 			padding: 20px;
 			align-items: center;
+
 			justify-content: center;
 			flex-direction: row;
-			fill: rgba(255, 255, 255, 0.7);
+
 			@include min-screen($desktop-md) {
 				top: 0;
 				left: 0;
@@ -56,7 +90,12 @@
 	.icon-container {
 		width: 20px;
 		height: 20px;
-
+		cursor: pointer;
+		text-decoration: none;
+		fill: rgba(255, 255, 255, 0.7);
+		&:visited {
+			fill: rgba(255, 255, 255, 0.7);
+		}
 		&:not(:last-child) {
 			margin-right: 10px;
 		}
